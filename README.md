2072014e2a9c4011acdb706c51a1220b6e26c885
rijal2 <merentas5benua27@gmail.com>
2022-02-07 02:00:47 +0700

    03. Memanfaatkan Arrow function renderHTML untuk menampilkan multi file html

--- README.md
+++ README.md
@@ -9,6 +9,14 @@ BUAT FILE HTML
 MEMANGGIL FILE HTML YANG SUDAH DIBUAT
 01. Karena file yang akan digunakan berada pada sistem operasi kita (local komputer), maka Gunakan core module FileSystem. Impor core module FileSystem dengan >> const fs = require('fs')
 02. Gunakan fs, lanjutkan dengan metode readFile()
-03. Isi parameternya dengan error dan data.
+03. Isi parameternya dengan err dan data. Err berisi informasi bahwa file yang direquest ada atau tidak. Jika tidak ada maka err berisi error. Data berisi file yang direquest.
 
 04. Cek isi err, apakan benar ada error?
+05. Isi perintah ketika terjadi error
+06. render data jika tidak errors
+
+=============================================================
+BIKIN FUNGSI UNTUK RENDER HTML AGAR BISA DIGUNAKAN UNTUK MERENDER BANYAK HTML
+
+01. Bikin arrow function bernama renderHTML yang berisi parameter path dan res
+02. Gunakan arrow function tersebut di dalam http.createServer()
--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const http = require('http');//memanggil Core Module http yang ada di NodeJS
 const { debug } = require('util');
 const port = 3000;
-const fs = require('fs')
+const fs = require('fs');
+
+const renderHTML = (path, res) => {
+    fs.readFile(path, ((err, data) => {//Parameter 'err' akan mengirimkan informasi file html yang dicari apakah file nya ada atau tidak. Jika tidak ada maka parameter 'err' akan berisi error.
+        if(err){//Jika err bernilai error/true
+            res.writeHead(404);
+            res.write(`Error: File yang anda cari tidak ditemukan`)
+        } else{//Jika file nya ada / tidak error
+            res.write(data);//parameter 'data' berisi data file html
+        }
+        res.end()
+    }))
+}
 
 //Membuat server cengan methode CreateServer(), kemudian jalankan dengan methode listen()
 http.createServer((req, res) => {
@@ -12,24 +24,11 @@ http.createServer((req, res) => {
     });
 
     if(url === '/about'){
-        res.write('Ini adalah halaman About');//Tulisan yang akan tampipl di halaman web
-        res.end();//>metode res.end() berfungsi untuk menghentikan proses yang ada di dalam server    
+        renderHTML('./about.html', res)    
     } else if(url === '/contact'){
-        res.write('Ini adalah halaman Contact');//Tulisan yang akan tampipl di halaman web
-        res.end();//>metode res.end() berfungsi untuk menghentikan proses yang ada di dalam server
+        renderHTML('./contact.html', res)
     } else{
-        fs.readFile('./index.html', ((err, data) => {//Parameter 'err' akan mengirimkan informasi file html yang dicari apakah file nya ada atau tidak. Jika tidak ada maka parameter 'err' akan berisi error.
-            if(err){//Jika err bernilai error/true
-                res.writeHead(404);
-                res.write(`Error: File yang anda cari tidak ditemukan`)
-            } else{//Jika file nya ada / tidak error
-                res.write(data);//parameter 'data' berisi data file html
-            }
-            res.end()
-        }))
-
-        res.write('Hello Word!');//Tulisan yang akan tampipl di halaman web
-        res.end();//>metode res.end() berfungsi untuk menghentikan proses yang ada di dalam server
+        renderHTML('./index.html', res)
     }
     
 }).listen(port, () => {
