886a91a7b68c43fbe5387a9edb858c0f820baea0
rijal2 <merentas5benua27@gmail.com>
2022-02-07 01:28:15 +0700

    02. Menggunakan core mOdule FileSystem, fs

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # WebServer2-DenganMultiHTML
 MENGHUBUNGKAN BEBERAPA HALAMAN HTML DENGAN WEB SERVER YANG SUDAH DIBUAT
+
+BUAT FILE HTML
+01. Membuat File About.html. Jadi setiap request yang mengarah pada 'localhost:3000/about' akan direspon dengan menampilkan halaman about.html
+02. Membuat File Contact.html. Jadi setiap request yang mengarah pada 'localhost:3000/contact' akan direspon dengan menampilkan halaman contact.html
+03. Membuat File index.html. Jadi rencananya setiap request menggunakan url apapun 'localhost:3000/apapun' maka akan direspon dengan menampilkan halaman index.html
+
+MEMANGGIL FILE HTML YANG SUDAH DIBUAT
+01. Karena file yang akan digunakan berada pada sistem operasi kita (local komputer), maka Gunakan core module FileSystem. Impor core module FileSystem dengan >> const fs = require('fs')
+02. Gunakan fs, lanjutkan dengan metode readFile()
+03. Isi parameternya dengan error dan data.
+
+04. Cek isi err, apakan benar ada error?
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http');//memanggil Core Module http yang ada di NodeJS
 const { debug } = require('util');
 const port = 3000;
+const fs = require('fs')
 
 //Membuat server cengan methode CreateServer(), kemudian jalankan dengan methode listen()
 http.createServer((req, res) => {
@@ -17,6 +18,16 @@ http.createServer((req, res) => {
         res.write('Ini adalah halaman Contact');//Tulisan yang akan tampipl di halaman web
         res.end();//>metode res.end() berfungsi untuk menghentikan proses yang ada di dalam server
     } else{
+        fs.readFile('./index.html', ((err, data) => {//Parameter 'err' akan mengirimkan informasi file html yang dicari apakah file nya ada atau tidak. Jika tidak ada maka parameter 'err' akan berisi error.
+            if(err){//Jika err bernilai error/true
+                res.writeHead(404);
+                res.write(`Error: File yang anda cari tidak ditemukan`)
+            } else{//Jika file nya ada / tidak error
+                res.write(data);//parameter 'data' berisi data file html
+            }
+            res.end()
+        }))
+
         res.write('Hello Word!');//Tulisan yang akan tampipl di halaman web
         res.end();//>metode res.end() berfungsi untuk menghentikan proses yang ada di dalam server
     }
